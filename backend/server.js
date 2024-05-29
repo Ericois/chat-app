5ccd7e00b911763431cdf7e121141e3a57fd6612
Ericois <47019178+Ericois@users.noreply.github.com>
2024-05-28 21:34:11 -0700

    Completed backend

--- backend/server.js
+++ backend/server.js
@@ -1,26 +1,35 @@
 import express from "express";
 import dotenv from "dotenv";
-const app = express();
 
-dotenv.config();
+import cookieParser from 'cookie-parser';
+
+import authRoutes from "./routes/auth.routes.js";
+import messageRoutes from "./routes/message.routes.js";
+import userRoutes from "./routes/user.routes.js";
+
+
+import connectToMongoDB from "./db/connectToMongoDB.js";
+
+const app = express();
 const PORT = process.env.PORT || 5000
 
-app.get("/", (req, res) => {
-  // root route http://localhost:5000/
-  res.send("Hello World!!");
+dotenv.config();
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+app.use("/api/users", userRoutes);
+
+
+//app.get("/", (req, res) => {
+// root route http://localhost:5000/
+//res.send("Hello World!!");
+//});
+
+
+app.listen(PORT, () => {
+  connectToMongoDB();
+  console.log(`Server running on port ${PORT}`);
 });
-
-app.get("/api/auth/signup", (req, res) => {
-  console.log("signup route");
-})
-
-app.get("/api/auth/login", (req, res) => {
-  console.log("login route");
-})
-
-app.get("/api/auth/logout", (req, res) => {
-  console.log("logout route");
-})
-
-
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
